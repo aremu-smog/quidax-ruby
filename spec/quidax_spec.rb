d730bae2cfd5c3dedc33f2e81ca644aaeefaeb5c
Aremu Oluwagbamila <aremuoluwagbamila@gmail.com>
2024-02-08 11:04:16 +0100

    test: spec for quidax.rb

--- spec/quidax_spec.rb
+++ spec/quidax_spec.rb
@@ -5,7 +5,24 @@ RSpec.describe Quidax do
     expect(Quidax::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "creates a valid object with secret key" do
+    quidax = Quidax::Quidax.new("s3cr3tk3y")
+    expect(quidax.secret_key).to eq("s3cr3tk3y")
+  end
+
+  it "should throw ArgumentError for too many arguments" do
+    begin
+      quidax = Quidax::Quidax.new("secret","another_key","unexpecte_argument")
+    rescue => e
+      expect(e.instance_of? ArgumentError).to eq(true)
+    end
+  end
+
+  it "should throw ArgumentError for too few arguments" do
+    begin
+      quidax = Quidax::Quidax.new
+    rescue => e
+      expect(e.instance_of? ArgumentError).to eq(true)
+    end
   end
 end
