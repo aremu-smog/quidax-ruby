86dcc2088e4c54f29c1181f0e6b7faea2604d97c
Aremu Oluwagbamila <aremuoluwagbamila@gmail.com>
2024-02-08 17:13:45 +0100

    test: specs for all subaccounts and account details argument error

--- spec/quidax_user_spec.rb
+++ spec/quidax_user_spec.rb
@@ -5,6 +5,14 @@ RSpec.describe QuidaxUser do
     quidax_object = Quidax::Quidax.new(test_secret_key)
     qUser = QuidaxUser.new(quidax_object)
 
+    it "raises ArgumentError for account details without id" do
+        begin
+            account_query = qUser.getAccountDetails
+        rescue => e
+            expect(e.instance_of? ArgumentError).to eq true
+        end
+
+    end
     it "should return user account details object" do
 
         account_query = qUser.getAccountDetails("me")
@@ -13,5 +21,13 @@ RSpec.describe QuidaxUser do
         account_details = account_query["data"]
         account_detail_fields = account_details.keys
         expect(account_detail_fields).to eq account_fields
+    end
+
+    it "gets all subaccounts" do
+        all_subaccounts_query = qUser.getAllSubAccounts
+        expect(all_subaccounts_query.nil?).to eq false
+
+        all_subaccounts = all_subaccounts_query["data"]
+        expect(all_subaccounts.is_a?(Array)).to eq true
     end
 end
