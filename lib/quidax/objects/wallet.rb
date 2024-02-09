4088eff28c6f641bd170fae587131c6d26fca03e
Aremu Oluwagbamila <aremuoluwagbamila@gmail.com>
2024-02-09 09:57:38 +0100

    feat: getWallet

--- lib/quidax/objects/wallet.rb
+++ lib/quidax/objects/wallet.rb
@@ -1,12 +1,21 @@
 class QuidaxWallet < QuidaxBaseObject
 
     def getAllWallets(account_id)
-        return QuidaxWallet.getAllWallets(account_id)
+        return QuidaxWallet.getAllWallets(@quidax,account_id)
+    end
+
+    def getWallet(account_id, currency)
+        return QuidaxWallet.getWallet(@quidax, account_id, currency)
     end
     
-    def self.getAllWallets(account_id)
+    def self.getAllWallets(qObject, account_id)
         path = "#{API::USER_PATH}/#{account_id}/wallets"
-        get_request(path)
+        get_request(qObject, path)
+    end
+
+    def self.getWallet(qObject, account_id, currency)
+        path ="#{API::USER_PATH}/#{account_id}/"
+        get_request(qObject, path)
     end
 
 
--- spec/quidax_wallet_spec.rb
+++ spec/quidax_wallet_spec.rb
@@ -1,9 +1,10 @@
 
 test_secret_key = ["TEST_SECRET_KEY"]
+wallet_fields = ["id","currency","balance","locked","staked","user","converted_balance","reference_currency","is_crypto","created_at","updated_at","deposit_address","destination_tag"]
 RSpec.describe QuidaxWallet do
     qObject = Quidax::Quidax.new(test_secret_key)
     qWallet = QuidaxWallet.new(qObject)
-    it "returns ArgumentError for getAllWallets without account_id" do
+    it "raises ArgumentError for getAllWallets without account_id" do
         begin
             all_wallets_query = qWallet.getAllWallets
         rescue => e
@@ -18,4 +19,21 @@ RSpec.describe QuidaxWallet do
         all_wallets = all_wallets_query["data"]
         expect(all_wallets.is_a?(Array)).to eq true
     end
+
+    it "raises ArgumentError for getWallet without any params" do
+        begin
+            wallet_query = qWallet.getWallet
+        rescue => e
+            expect(e.instance_of? ArgumentError).to eq true
+        end
+    end
+
+    it "returns wallet address for getWallet with params" do
+        wallet_query = qWallet.getWallet("me","btc")
+        expect(wallet_query["data"].nil?).to eq false
+        
+        wallet = wallet_query["data"]
+        expect(wallet.keys).to eq wallet_fields
+    end
+
 end
