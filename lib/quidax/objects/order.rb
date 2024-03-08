d1ab6c017cbafe60039928ecd2a6a8085f5def85
Aremu Oluwagbamila <aremuoluwagbamila@gmail.com>
2024-03-08 18:31:37 +0100

    refactor: order

--- lib/quidax/objects/order.rb
+++ lib/quidax/objects/order.rb
@@ -2,46 +2,48 @@
 
 # Order object
 class QuidaxOrder < QuidaxBaseObject
-  def getAll(user_id:, market:, state:, order_by:)
-    QuidaxOrder.getAll(q_object: @quidax, user_id: user_id, market: market, order_by: order_by, state: state)
+  def get_all(user_id:, query:)
+    QuidaxOrder.get_all(q_object: @quidax, user_id: user_id, query: query)
   end
 
-  def create(user_id:, data:)
-    QuidaxOrder.create(q_object: @quidax, user_id: user_id, data: data)
+  def create(user_id:, body:)
+    QuidaxOrder.create(q_object: @quidax, user_id: user_id, body: body)
   end
 
   def cancel(user_id:, order_id:)
     QuidaxOrder.cancel(q_object: @quidax, user_id: user_id, order_id: order_id)
   end
 
-  def getDetails(user_id:, order_id:)
-    QuidaxOrder.getDetails(q_object: @quidax, user_id: user_id, order_id: order_id)
+  def get_details(user_id:, order_id:)
+    QuidaxOrder.get_details(q_object: @quidax, user_id: user_id, order_id: order_id)
   end
 
-  def self.create(q_object:, user_id:, data:) # rubocop:disable Metrics/AbcSize
+  def self.get_all(q_object:, user_id:, query:)
+    query.stringify_keys!
+    Utils.check_missing_keys(required_keys: %w[market], keys: query.keys, field: "query")
+    query["order_by"] = "asc" unless query.include?("order_by")
+    query["state"] = "done" unless query.include?("state")
+    Utils.validate_value_in_array(array: %w[asc desc], value: query["order_by"], field: 'query["order_by"]')
+    Utils.validate_value_in_array(array: %w[done wait cancel], value: query["state"], field: 'query["state"]')
+
     path = "#{API::USER_PATH}/#{user_id}#{API::ORDER_PATH}"
 
+    get_request(q_object, path, query)
+  end
+
+  def self.create(q_object:, user_id:, body:)
+    body.stringify_keys!
+
     expected_data_keys = %w[market side ord_type price volume]
-    data.transform_keys!(&:to_s)
-    data_keys = data.keys
-    diff_in_data_keys = expected_data_keys - data_keys.map(&:to_s)
-    missing_keys = diff_in_data_keys.join(", ")
-    has_missing_keys = diff_in_data_keys.empty?
-    raise ArgumentError, "#{missing_keys} are missing in :data" unless has_missing_keys
+    Utils.check_missing_keys(required_keys: expected_data_keys, keys: body.keys, field: "body")
 
-    allowed_sides = %w[buy sell]
-    unless allowed_sides.include?(data["side"])
-      raise ArgumentError,
-            "data['side'] must be one of #{allowed_sides.join(", ")}"
-    end
+    Utils.validate_value_in_array(array: %w[buy sell], value: body["side"], field: 'body["side"]')
+    body["ord_type"] = "limit" unless body.include?("ord_type")
+    Utils.validate_value_in_array(array: %w[limit market], value: body["ord_type"], field: 'body["ord_type"]')
 
-    allowed_ord_type = %w[limit market]
-    unless allowed_ord_type.include?(data["ord_type"])
-      raise ArgumentError,
-            "data['ord_type'] must be one of #{allowed_ord_type.join(", ")}"
-    end
+    path = "#{API::USER_PATH}/#{user_id}#{API::ORDER_PATH}"
 
-    post_request(q_object, path, data)
+    post_request(q_object, path, body)
   end
 
   def self.cancel(q_object:, user_id:, order_id:)
@@ -50,26 +52,7 @@ class QuidaxOrder < QuidaxBaseObject
     post_request(q_object, path)
   end
 
-  def self.getAll(q_object:, user_id:, market:, state:, order_by:)
-    allowed_order_values = %w[asc desc]
-    order_by ||= "asc"
-    unless allowed_order_values.include?(order_by.downcase)
-      raise ArgumentError,
-            ":order_by must be one of: #{allowed_order_values.join(", ")}"
-    end
-
-    path = "#{API::USER_PATH}/#{user_id}#{API::ORDER_PATH}"
-
-    params = {
-      market: market,
-      state: state,
-      order_by: order_by
-    }
-
-    get_request(q_object, path, params)
-  end
-
-  def self.getDetails(q_object:, user_id:, order_id:)
+  def self.get_details(q_object:, user_id:, order_id:)
     path = "#{API::USER_PATH}/#{user_id}#{API::ORDER_PATH}/#{order_id}"
 
     get_request(q_object, path)
